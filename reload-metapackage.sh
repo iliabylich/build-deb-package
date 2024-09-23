ea3c4ee431a3f1d882eb0a68be5150dc008c9ade
Ilya Bylich <ibylich@gmail.com>
2024-09-23 21:47:10 +0200

    simplify reload-packages.sh

--- reload-metapackage.sh
+++ reload-metapackage.sh
@@ -4,9 +4,4 @@ set -eu
 
 PACKAGES=$(cat metapackage.yml | yq -r ".debian.dependencies | join(\" \")")
 
-for PACKAGE in $PACKAGES; do
-    if [ "$PACKAGE" != '${shlibs:Depends}' ]
-    then
-        sudo apt-mark auto $PACKAGE
-    fi
-done
+sudo apt-mark auto $PACKAGES
